efca86decf87b5257dc410cd41f5103e44f13d8c
Ravitej Mulay <ravitejmulay93@gmail.com>
2025-03-15 01:51:53 +0530

    Initial commit

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 const db = mysql.createConnection({
-  host : "localhost",
-  user : "root",
-  password : "root",
-  database : "practicedb"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
+
 
 db.connect((err) => {
   if (err) throw err;
