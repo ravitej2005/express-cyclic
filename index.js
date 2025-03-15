ce72eb0f4bdd3f1f3df8c61f45e4a132c6c0ef93
Ravitej Mulay <ravitejmulay93@gmail.com>
2025-03-15 19:08:53 +0530

    express-hosting

--- index.js
+++ index.js
@@ -11,12 +11,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
+const db =  mysql.createConnection(process.env.DB_URL);
 
 
 db.connect((err) => {
